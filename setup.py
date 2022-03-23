275bf800900db525f449e127909167627fdaae5f
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2022-03-23 12:45:18 +0100

    fix license statement

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3 :: Only',
         'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
